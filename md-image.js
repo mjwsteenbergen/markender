961ca28d4b6c8f941582b8ec0b604bf124a42c46
Martijn Steenbergen <martijnsreclame@hotmail.nl>
2018-03-12 21:30:23 +0100

    Better linking

---
+++ md-image.js
@@ -0,0 +1,186 @@
+// Process ![image](<src> "title")
+
+'use strict';
+
+function isSpace(code) {
+  switch (code) {
+    case 0x09:
+    case 0x20:
+      return true;
+  }
+  return false;
+}
+
+function normalizeReference(str) {
+  // use .toUpperCase() instead of .toLowerCase()
+  // here to avoid a conflict with Object.prototype
+  // members (most notably, `__proto__`)
+  return str
+    .trim()
+    .replace(/\s+/g, ' ')
+    .toUpperCase();
+}
+
+module.exports = function linkDing(md, options) {
+  md.renderer.rules.mdimage = mdImage;
+  md.inline.ruler.after('escape', 'linker', mdImage);
+  //console.error("hi");
+};
+
+function mdImage(state, silent) {
+  var attrs,
+    code,
+    content,
+    label,
+    labelEnd,
+    labelStart,
+    pos,
+    ref,
+    res,
+    title,
+    token,
+    tokens,
+    start,
+    href = '',
+    oldPos = state.pos,
+    max = state.posMax;
+
+  if (state.src.charCodeAt(state.pos) !== 0x21 /* ! */) {
+    return false;
+  }
+  if (state.src.charCodeAt(state.pos + 1) !== 0x5b /* [ */) {
+    return false;
+  }
+
+  labelStart = state.pos + 2;
+  labelEnd = state.md.helpers.parseLinkLabel(state, state.pos + 1, false);
+
+  // parser failed to find ']', so it's not a valid link
+  if (labelEnd < 0) {
+    return false;
+  }
+
+  pos = labelEnd + 1;
+  if (pos < max && state.src.charCodeAt(pos) === 0x28 /* ( */) {
+    //
+    // Inline link
+    //
+
+    // [link](  <href>  "title"  )
+    //        ^^ skipping these spaces
+    pos++;
+    for (; pos < max; pos++) {
+      code = state.src.charCodeAt(pos);
+      if (!isSpace(code) && code !== 0x0a) {
+        break;
+      }
+    }
+    if (pos >= max) {
+      return false;
+    }
+
+    // [link](  <href>  "title"  )
+    //          ^^^^^^ parsing link destination
+    start = pos;
+    res = state.md.helpers.parseLinkDestination(state.src, pos, state.posMax);
+    if (res.ok) {
+      href = state.md.normalizeLink(res.str);
+      if (state.md.validateLink(href)) {
+        pos = res.pos;
+      } else {
+        href = '';
+      }
+    }
+
+    // [link](  <href>  "title"  )
+    //                ^^ skipping these spaces
+    start = pos;
+    for (; pos < max; pos++) {
+      code = state.src.charCodeAt(pos);
+      if (!isSpace(code) && code !== 0x0a) {
+        break;
+      }
+    }
+
+    // [link](  <href>  "title"  )
+    //                  ^^^^^^^ parsing link title
+    res = state.md.helpers.parseLinkTitle(state.src, pos, state.posMax);
+    if (pos < max && start !== pos && res.ok) {
+      title = res.str;
+      pos = res.pos;
+
+      // [link](  <href>  "title"  )
+      //                         ^^ skipping these spaces
+      for (; pos < max; pos++) {
+        code = state.src.charCodeAt(pos);
+        if (!isSpace(code) && code !== 0x0a) {
+          break;
+        }
+      }
+    } else {
+      title = '';
+    }
+
+    if (pos >= max || state.src.charCodeAt(pos) !== 0x29 /* ) */) {
+      state.pos = oldPos;
+      return false;
+    }
+    pos++;
+  } else {
+    //
+    // Link reference
+    //
+    if (typeof state.env.references === 'undefined') {
+      return false;
+    }
+
+    if (pos < max && state.src.charCodeAt(pos) === 0x5b /* [ */) {
+      start = pos + 1;
+      pos = state.md.helpers.parseLinkLabel(state, pos);
+      if (pos >= 0) {
+        label = state.src.slice(start, pos++);
+      } else {
+        pos = labelEnd + 1;
+      }
+    } else {
+      pos = labelEnd + 1;
+    }
+
+    // covers label === '' and label === undefined
+    // (collapsed reference link and shortcut reference link respectively)
+    if (!label) {
+      label = state.src.slice(labelStart, labelEnd);
+    }
+
+    ref = state.env.references[normalizeReference(label)];
+    if (!ref) {
+      state.pos = oldPos;
+      return false;
+    }
+    href = ref.href;
+    title = ref.title;
+  }
+
+  //
+  // We found the end of the link, and know for a fact it's a valid link;
+  // so all that's left to do is to call tokenizer.
+  //
+  if (!silent) {
+    content = state.src.slice(labelStart, labelEnd);
+
+    state.md.inline.parse(content, state.md, state.env, (tokens = []));
+
+    token = state.push('image', 'md-img', 0);
+    token.attrs = attrs = [['src', href], ['alt', '']];
+    token.children = tokens;
+    token.content = content;
+
+    if (title) {
+      attrs.push(['title', title]);
+    }
+  }
+
+  state.pos = pos;
+  state.posMax = max;
+  return true;
+}
--- src/script/md-bib.ts
+++ src/script/md-bib.ts
@@ -23,18 +23,22 @@ export class Bibliography extends HTMLElement{
         header.innerHTML = "Bibliography";
         this.appendChild(header);
 
-        document.addEventListener("DOMContentLoaded", function(event) {
-            bib.startAlgorithm();
+        
+        document.addEventListener("readystatechange", function(event) {
+            if(document.readyState !== "complete") return;
+            var storage = bib.getLinkStorageOrCreate();
+            bib.startAlgorithm(storage);
         });
 
         // this.appendChild(this.template);
         
     }
 
-    startAlgorithm() {
+    startAlgorithm(storage) {
         var httpRequest = new XMLHttpRequest();
         var bib = this;
         var once = false;
+
         httpRequest.onreadystatechange = function(e){
             if (httpRequest.status === 200 && httpRequest.responseText !== "") {
                 if(once) {
@@ -43,7 +47,17 @@ export class Bibliography extends HTMLElement{
                 once = true;
                 var bibtex = httpRequest.responseText;
                 bib.bibtex = new BibtexParser().toJSON(bibtex);
-                bib.replaceText(bib.parentNode.parentNode,  new Map<string, number>());
+                var number = 1;
+                console.log(bib.bibtex[0]);
+                bib.bibtex.forEach(function(bibitem){
+                    if(bibitem instanceof BibtexEntry){
+                        var name = bib.format.replace("{refnumber}", number.toString());
+                        storage.setLink(bibitem.citationKey, name, "#bib-item-" + number)
+                        bib.addBibItem(bibitem, number, bib);
+                        number++;                                 
+                    }
+                });
+                console.log("done");
             }
             else {
                 console.error(httpRequest.status);  
@@ -53,74 +67,32 @@ export class Bibliography extends HTMLElement{
         httpRequest.send();            
     }
 
-    replaceText(node: Node, refs: Map<string, number>) {
-        var bib = this;
-        Array.from(node.childNodes).forEach(function(element) {
-            //Replace if text
-            if (element.nodeName == "#text")
-            {
-                refs = this.replaceTextNode(element, refs);
-            }
-
-            //Continue down searchtree if not
-            if (!this.excludeElements.includes(element.nodeName)) {
-                refs = this.replaceText(element, refs);    
-            }
-        }, this);  
-        return refs;      
-    }
-
-    replaceTextNode(element: Node, refs: Map<string,number>): Map<string, number>
-    {
-        var regex = /\[([^[]+)\]/g;
-        var output = regex.exec(element.textContent);
-        let bib = this;
-        if(output != null)
-        {
-            let refelement: Node = document.createTextNode(output[0]);
-            this.bibtex.forEach(function(bibitem){
-                if(bibitem instanceof BibtexEntry && bibitem.citationKey == output[1]){
-                    var ref = refs.get(bibitem.citationKey); 
-
-                    //If it is new
-                    if (ref === undefined) {
-                        refs.set(bibitem.citationKey, refs.size + 1);
-                        bib.addBibItem(bibitem, refs.size);                                 
-                    }
-
-                    let actualRef = document.createElement("a");
-                    actualRef.innerHTML =  bib.formatBib(bibitem, refs.get(bibitem.citationKey));
-                    refelement = actualRef;                        
-                }
-            });
-            let parent = element.parentNode;
-            let textArray:string[] = element.textContent.split(output[0]);
-            let rightTextArray = textArray.concat([]);
-            rightTextArray.shift();
-            console.log(rightTextArray.join(output[0]));
-            let textNode2 = document.createTextNode(rightTextArray.join(output[0]));
-            parent.replaceChild(textNode2,element);
-            console.log(textArray);
-            parent.insertBefore(refelement, textNode2);
-            parent.insertBefore(document.createTextNode(textArray[0]), refelement);
-            bib.replaceTextNode(textNode2, refs);   
-        }
-        return refs;
-    }
-
     formatBib(bibitem, refnumber)
     {
         var res = this.format.replace("{refnumber}", refnumber);
         return "<a href=\"" + "#bib-item-" + refnumber +"\">" + res + "</a>";
     }
 
-    addBibItem(bibitem, refnumber)
+    getLinkStorageOrCreate() {
+        var storage = document.getElementsByTagName("md-link-storage");
+        if(storage.length > 0) {
+            return storage[0];
+        } else {
+            // var storage = document.createElement("md-link-storage");
+            // document.body.appendChild(storage);
+            return storage;
+        }
+
+    }
+
+    addBibItem(bibitem, refnumber, bib)
     {
         var item = document.createElement('md-bib-item');
         item.setAttribute("bibitem", JSON.stringify(bibitem));
         item.setAttribute("refnumber", refnumber);
+        item.setAttribute("name", this.formatBib(bibitem, refnumber));
 
-        this.appendChild(item);
+        bib.appendChild(item);
     }
 }
 
