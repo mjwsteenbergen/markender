f13eaba6d487012dc9349da9da10c2b204c34994
Martijn Steenbergen <m.uses+git@outlook.com>
2018-09-16 22:18:05 +0200

    do not try to do fancy stuff with style. Just update body class

--- src/components/md-style/md-style.ts
+++ src/components/md-style/md-style.ts
@@ -1,19 +1,7 @@
 export class MdStyle extends HTMLElement {
     
     connectedCallback() {
-        var s = document.getElementById("default-styles");
-        if (s !== null) {
-            s.remove();
-        }
-
-        document.querySelectorAll("style").forEach(i => {
-            if (i.innerHTML.includes(".vscode")) {
-                i.remove();
-            }
-        });
-
         var name = this.getAttribute("name");
-
-        document.body.className = "scrollBeyondLastLine wordWrap showEditorSelection " + name;
+        document.body.className = name;
     }
 }
