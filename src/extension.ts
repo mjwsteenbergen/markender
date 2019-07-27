127102c688f0f24d24fc1b15660adefd195b9e3e
Martijn Steenbergen <m.uses+git@outlook.com>
2019-07-27 09:44:43 +0200

    Override something

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext } from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     // console.log('Congratulations, your extension "markender" is now active!');
